af90a203d095d7c1744ea0d8fac063723c419238
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2023-08-10 19:38:06 -0300

    Metodo get clientes

--- app/cadastrocliente-app/src/main/java/com/cadastrocliente/cadastroclienteapp/controller/ClienteController.java
+++ app/cadastrocliente-app/src/main/java/com/cadastrocliente/cadastroclienteapp/controller/ClienteController.java
@@ -5,23 +5,39 @@ import com.cadastrocliente.cadastroclienteapp.model.entity.Cliente;
 import com.cadastrocliente.cadastroclienteapp.service.ClienteService;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
 public class ClienteController {
 
     private ClienteService service;
 
     private ModelMapper modelMapper;
 
+
     public ClienteController(ClienteService service, ModelMapper modelMapper){
         this.service = service;
         this.modelMapper = modelMapper;
     }
 
+    @PostMapping("/cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public ClienteDTO createCliente(@RequestBody @Valid ClienteDTO cliente){
         Cliente entity = modelMapper.map(cliente, Cliente.class);
         entity = service.save(entity);
         return modelMapper.map(entity, ClienteDTO.class);
+
+    }
+
+    @GetMapping("/clientes")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ClienteDTO> getCliente (){
+        List<Cliente> clienteList = this.service.getList();
+        return clienteList.stream().map(cliente -> modelMapper.map(cliente, ClienteDTO.class)).collect(Collectors.toList());
 
     }
 }
--- app/cadastrocliente-app/src/main/java/com/cadastrocliente/cadastroclienteapp/service/ClienteService.java
+++ app/cadastrocliente-app/src/main/java/com/cadastrocliente/cadastroclienteapp/service/ClienteService.java
@@ -2,9 +2,15 @@ package com.cadastrocliente.cadastroclienteapp.service;
 
 import com.cadastrocliente.cadastroclienteapp.model.entity.Cliente;
 
+import java.util.List;
+
 public interface ClienteService {
 
     Cliente save(Cliente any);
+
+    List<Cliente> getList();
+
+
 
 
 }
